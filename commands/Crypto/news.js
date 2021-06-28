e829a6ba6cc8862473fa140cf7108c4c0f1ee932
AdamT20054 <AdamTOneill05@protonmail.com>
2021-06-28 22:38:06 +0100

    pat help <3

--- commands/Crypto/news.js
+++ commands/Crypto/news.js
@@ -1,5 +1,4 @@
 // @ts-check
-
 const { Command, Parameter } = require('@pat.npm.js/discord-bot-framework');
 const { noop } = require('../../util.js');
 const axios = require('axios');
@@ -13,8 +12,8 @@ module.exports = new Command()
     .setType('Guild')
     .addParameters(
         new Parameter()
-            .setKey('coin')
-            .setDescription('Choose what coin you want the bot to find news articles for.')
+            .setKey('query')
+            .setDescription('Choose what query you want the bot to find news articles for.')
             .setRequired(false),
         new Parameter()
             .setKey('language')
@@ -24,20 +23,24 @@ module.exports = new Command()
     .addPermissions('SEND_MESSAGES')
     .setCallback(async function(message, args, client) {
         try {
-            
-            
+
+
+            // Takes input for langauge and converts it to lowercase so the API can use it, or defaults to 'en' if no input is given.
             const languagee = args.get('language')?.value || 'en';
             const language = languagee.toLowerCase();
 
 
-            const coin = args.get('coin')?.value || `Defi`;
+            // Takes the string 'query' from the command to use in the API, if no coin is specified then its default query will be 'DeFi'.
+            const query = args.get('query')?.value || `DeFi`;
 
+
+            // Generate results for the given search query based on user inputs and saves it as the const 'data' to be used next.
             const { data } = await axios.get(
-              `https://newsapi.org/v2/everything?q=${coin}&apiKey=${process.env.NEWS_API_KEY}&pageSize=1&sortBy=publishedAt&language=${language}`
+              `https://newsapi.org/v2/everything?q=${query}&apiKey=${process.env.NEWS_API_KEY}&pageSize=1&sortBy=publishedAt&language=${language}`
             );
-            //console.log(`https://newsapi.org/v2/everything?q=${coin}&apiKey=${process.env.NEWS_API_KEY}&pageSize=1&sortBy=publishedAt&language=${language}`)
-      
-            // Destructure useful data from response
+            // If this bit is red on VisualStudio dont worry, it still works 100% fine if you haven't changed anything that may of broken it.
+
+            // Destructure useful data from the first article provided by the response (data.articles[0]) and turn them into usable variables for our embed response.
             const {
               title,
               source: { name },
@@ -45,33 +48,30 @@ module.exports = new Command()
               url,
               urlToImage,
               publishedAt,
-              content,
             } = data.articles[0];
-          let coinUP = coin.toUpperCase()
-      
+            // data.articles[0] is the first article, data.articles[1] is the 2nd, [3] is the 3rd and so on...
+
+
+          // Construct the embed.
           let embed = new MessageEmbed()
             .setColor('#0099ff')
             .setTitle(`${title}`)
             .setURL(url)
             .setAuthor(`Lyckas News`)
             .setDescription(`*${description}*`)
-            //.addField({ name: `Published at:`, value: `${publishedAt}`, inline: true })
-            //.setThumbnail(urlToImage)
-            .addFields(
-                //{ name: 'Article description:', value: `${description}` },
-                //{ name: '----------------------' },
-                { name: 'Article Content:', value: `${content}`, inline: true },
-                //{ name: 'Inline field title', value: 'Some value here', inline: true },
-            )
-            //.addField('Inline field title', 'Some value here', true)
             .setImage(urlToImage)
             .setTimestamp()
-            .setFooter(`${name}`, 'https://gatehub.net/blog/content/images/2020/05/Crypto-desctiption-3.jpg');
-            
+            .setFooter(`${name} | Published at ${publishedAt}`, 'https://gatehub.net/blog/content/images/2020/05/Crypto-desctiption-3.jpg');
+          
+          // Send the embed when the command is called.
           message.reply({ embeds: [embed], allowedMentions: { parse: [] } });
-          //console.log(urlToImage)
+
+          // Catch any errors to stop the bot from crashing and going offline.
           } catch (err) {
             console.log(err)
-            return message.reply('There was an error, rate limited by API? Please try again later.');
+            // Alert the user there was an error VIA a reply message.
+            return message.reply(
+              `**There was an error :(**\nCheck your query is **realistic**, maybe there are just no results? Try using a differen't language for your query?\n\nIf there are no results then consider joining the *support server* (**!support**) to get help and help us solve the issue!`
+              );
           }
         })
