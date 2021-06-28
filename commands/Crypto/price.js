76ff3c05f4c48aa5d4019d9606caf44eb10848ca
AdamT20054 <AdamTOneill05@protonmail.com>
2021-06-29 00:02:55 +0100

    sometimes it may be good sometimes it may bea shiit

--- commands/Crypto/price.js
+++ commands/Crypto/price.js
@@ -1,7 +1,9 @@
 // @ts-check
-const { default: axios } = require('axios');
+const axios = require('axios');
 const { Command, Parameter } = require('@pat.npm.js/discord-bot-framework');
 const { noop } = require('../../util.js');
+const { MessageEmbed } = require('discord.js');
+
 
 module.exports = new Command()
     .setName('price')
@@ -18,19 +20,48 @@ module.exports = new Command()
     )
     .addPermissions('SEND_MESSAGES')
     .setCallback(async function(message, args, client) {
-        const coin = args.first().value.toLowerCase();
 
+        // Takes the 'coin' value and makes it lowercase for the API to use.
+        const coin = args.first().value.toLowerCase();
+        
+        // Takes the 'currency' value and assignes it as the 'VSCurrency' variables value, or uses usd as the 'VSCurrency' variables value if no value is provided
         const VSCurrency = args.get('currency')?.value.toLowerCase() || 'usd';
 
-        const res = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=${VSCurrency}`).catch(noop);
+        // Grabbing all the data we need from the links and assigning each link's data a variable to use for our reply.
+        
+        const info = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${VSCurrency}&ids=${coin}&order=market_cap_desc&per_page=100&page=1&sparkline=false`).catch(noop);        
+        // 
 
-        if (!res)
-            return message.reply(`**Could not find the coin you're looking for :(**\nMake sure you use its full name [Bitcoin, not BTC] and if the issue still persists, do !support to join the support server!`).catch(noop);
 
-        const { data } = res;
 
+        // Checking to see if the coin provided is valid, this is the only check we need as the data is grabbed from the same variables in the URL.
+        if (!info)
+            return message.reply(`**Could not find the coin you're looking for :(**\nMake sure you use its full name [Bitcoin, not BTC] and the currency is valid. If the issue still persists, do !support to join the support server!`).catch(noop);
+
+
+        // Turning our raw data provided by the links into usable data.
+        const { data } = info;
+        
+
+
+        // Checks to see if it has been given data to present, if no data is here then it will give an error message in chat.
         if (!data[coin]?.[VSCurrency])
-            return message.reply('Unable to fetch prices. Are you sure that coin and fiat pair exist?\nHere is an example of the command: *!price Bitcoin gbp*\n\n**PLEASE NOTE:** Abreviations are not implemented yet, please use the currencys full name.').catch(noop);
+            return message.reply('Unable to fetch prices. Are you sure that coin and fiat pair exist?\nHere is an example of the command: *!price Bitcoin gbp*\n\n**PLEASE NOTE:** Abreviations are not implemented yet, please use the currencies full name.').catch(noop);
 
-        message.reply(`Current price of ${coin}***:***  ${data[coin][VSCurrency]} ${VSCurrency.toUpperCase()}`).catch(noop);
+
+        // Constructing the embed that will hold the data and present it to the chat
+        let embed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`title`)
+            .setURL('google.com')
+            .setAuthor(`Lyckas News`)
+            .setDescription(`*descriptio}*`)
+            //.setImage(urlToImage)
+            .setTimestamp()
+            .setFooter(`footer`, 'https://gatehub.net/blog/content/images/2020/05/Crypto-desctiption-3.jpg');
+
+        
+        // Send the embed when the command is called.
+        message.reply({ embeds: [embed], allowedMentions: { parse: [] } }).catch(noop);
+        
     });
