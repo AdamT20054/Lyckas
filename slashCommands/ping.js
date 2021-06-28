e07632aa2bf26ce5249176cd9c7e256b5c2c398f
Patrick Lynch <patrick.2004.lynch@gmail.com>
2021-06-28 15:56:23 +0100

    slash command

---
+++ slashCommands/ping.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+const { SlashCommand, SlashCommandOption } = require('@pat.npm.js/discord-bot-framework');
+
+module.exports = new SlashCommand()
+    .setName('ping')
+    .setDescription('Ping pong!')
+    .setGuild('759176740429496332')
+    .addOptions(
+        new SlashCommandOption()
+            .setName('name')
+            .setDescription('Your name!')
+            .setRequired(true)
+            .setType('STRING')
+    )
+    .setCallback(function(interaction, command, client) {
+        interaction.reply(`Pong, ${interaction.options.first().value}!`);
+    });
